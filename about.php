925d5acd77ceefa13c6ce27a8ac5de3ea2135ac0
Brenden Smith <smitbre2@oregonstate.edu>
2019-08-05 16:19:32 -0700

    Stand in about file

---
+++ about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<?php
+	include 'connectvars.php';
+?>
+
+<html>
+
+<!--INCLUDE SCRIPTS HERE-->
+
+   <link rel="stylesheet" href="main.css">
+
+   <head>
+	<title>Movies R Us</title>
+   </head>
+
+   <body>
+	<div class = "navbar">
+		<a class="active" href="index.php">Home</a>
+		<a href="inventory.php">Inventory</a>
+		<a href="reviews.php">Reviews</a>
+		<a href="about.php">About</a>
+	</div>
+
+	<h3>About</h3>
+   </body>
+</html>
