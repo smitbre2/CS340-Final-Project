061dcd806c3909eef056588063531a0017936315
Brenden Smith <smitbre2@oregonstate.edu>
2019-08-05 15:08:21 -0700

    DB Acess form
    It's blank, so fill it out

---
+++ connectvars.php
@@ -0,0 +1,7 @@
+<?php 
+	define('DB_HOST', '');
+	define('DB_USER', '');
+	define('DB_PASSWORD', '');
+	define('DB_NAME', '');
+	define('CON_STRING', 'mysql:host=classmysql.engr.oregonstate.edu;dname=cs340_username');
+?>
