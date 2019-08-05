21f85274932e2b4bd68c94a6dbcd4ed73c1ae111
Brenden Smith <smitbre2@oregonstate.edu>
2019-08-05 15:42:38 -0700

    Added php table
    Has not been tested

--- inventory.php
+++ inventory.php
@@ -7,3 +7,42 @@
 	<h3>Inventory</h3>
 	<link rel="stylesheet" href="main.css">
 </html>
+
+
+<?php
+	//Create connect token for database
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if(!$conn) {
+		die('Could not connect: ' . msql_error());
+	}
+
+	//Get table from database
+	$query = "SELECT * FROM Inventory"
+	$result = mysqli_query($conn, $query);
+	if(!$result) {
+		die("Query to table failed");
+	}
+	
+	//Print Column Names
+	$col_num = mysli_num_fields($result);
+	echo "<h1>Inventory</h1>";
+	echo "<table border ='1'><tr>";
+
+	for($i=0; $i<$col_num; $i++) {
+		//Get column names
+		$field = mysqli_fetch_field($result);
+		echo "<td><b>{$field->name}</b></td>";
+	}
+
+	//Print each individual row out
+	echo "</tr>\n";
+	while($row = msqli_fetch_row($result)) {
+
+		echo "<tr>";
+		foreach($row as $cell)
+			echo "<td>$cell</td>";
+		echo "</tr>\n";
+	}
+
+	}
+?>
