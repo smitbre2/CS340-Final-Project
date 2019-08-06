e9b41b0cb7214583e64675ba837cac4e85572192
Brenden Smith <smitbre2@flip3.engr.oregonstate.edu>
2019-08-05 17:12:58 -0700

    Fixed css error with cart

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 		<a href="inventory.php">Inventory</a>
 		<a href="reviews.php">Reviews</a>
 		<a href="about.php">About</a>
-		<a class="right" href="cart.php">Checkout</a>
-	</div>
+		<a href="cart.php" style="float:right">Checkout</a>
+</div>
 
 </header>
