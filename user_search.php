b730828ee41c71cb1e49be9b7f85deabd9a2091d
Brenden Smith <smitbre2@flip1.engr.oregonstate.edu>
2019-08-10 11:56:50 -0700

    User page for singular movie detail. Modular for all movies in DB

---
+++ user_search.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+   <?php
+	include 'connectvars.php';
+	include 'header.php';
+   ?>
+<html>
+	<head>
+		<link href="main.css" rel="stylesheet" type="text/css">
+	</head>
+<body>
+
+<?php
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if(!$conn) {
+		die('Could not connect: ' . msql_error());
+	}
+
+	$id = $_GET['id'];
+	$query = "SELECT * FROM Movie WHERE MOVIE_ID = $id";
+	$result = mysqli_query($conn, $query);
+
+	if(!$result) {
+		die("Query to show fields from table failed");
+	}
+	
+	$movie = mysqli_fetch_array($result);
+	echo "<div class='card'>";
+	
+	$img = file_get_contents('imgs/'. $movie["NAME"]);
+	echo "<img src=$img";
+	echo "style='width:25%'>";
+	echo "<div class='container'>";
+	echo "<h3><b>";
+	echo $movie["NAME"];
+	echo "</b></h3>";
+	echo '<p>';
+	echo $movie["DESCRIPTION"];
+        echo '</p>';
+	echo "</div>";
+	echo "</div>";
+?>
+
+
+</body>
+</html>
