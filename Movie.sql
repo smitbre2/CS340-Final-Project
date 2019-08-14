5cce5f4aa0e09b2e846529fa416e665b7e0b4a71
Thomas Mahler <mahlert@flip3.engr.oregonstate.edu>
2019-08-14 16:41:47 -0700

    added new SQL files

--- Movie.sql
+++ Movie.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: classmysql.engr.oregonstate.edu:3306
--- Generation Time: Jul 26, 2019 at 10:12 AM
+-- Generation Time: Aug 14, 2019 at 04:14 PM
 -- Server version: 10.3.13-MariaDB-log
 -- PHP Version: 7.0.33
 
@@ -45,7 +45,48 @@ INSERT INTO `Movie` (`MOVIE_ID`, `NAME`, `DESCRIPTION`, `YEAR`, `RATING`, `CATEG
 (8, 'Yesterday', 'Movie about musician and The Beatles.', 2019, 4, 'Drama'),
 (14, 'Once Upon A Time In Hollywood', 'A Movie about an actor and their stunt double trying to find roles in Golden Age Hollywood', 2019, 5, 'Drama'),
 (19, 'The Lion King', 'Movies about animals.', 2019, 4, 'Kids'),
-(66, 'Crawl', 'Movie about a family in a hurricane', 2019, 3, 'Thriller');
+(66, 'Crawl', 'Movie about a family in a hurricane', 2019, 3, 'Thriller'),
+(121, 'The Wizard of Oz', 'Journey to find the Wizard', 1939, 4, 'Fantasy'),
+(161, 'The Godfather', 'Movie about organized crime family', 1972, 5, 'Drama'),
+(190, 'Top Gun', 'The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills', 1986, 5, 'Drama'),
+(331, 'Looper', 'In a future society, time-travel exists, but it\'s only available to those with the means to pay for it on the black market.', 2012, 2, 'Action'),
+(434, 'Ferris Bueller\'s Day Off', 'Ferris Bueller has an uncanny skill at cutting classes and getting away with it.', 1986, 5, 'Drama'),
+(475, 'Titanic', 'Movie about the Titanic sinking', 1997, 5, 'Drama'),
+(641, 'The Bourne Identity', 'The story of a man, salvaged, near death, from the ocean by an Italian fishing boat. When he recuperates, the man suffers from total amnesia, without identity or background.', 2002, 5, 'Action');
+
+--
+-- Triggers `Movie`
+--
+DELIMITER $$
+CREATE TRIGGER `dec_cat` BEFORE DELETE ON `Movie` FOR EACH ROW BEGIN
+UPDATE Categories
+	SET MOVIES_IN_CAT = MOVIES_IN_CAT - 1
+	WHERE CATEGORY = (SELECT DISTINCT CATEGORY FROM Movie WHERE Movie.CATEGORY=old.CATEGORY);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `incr_cat` AFTER INSERT ON `Movie` FOR EACH ROW BEGIN
+UPDATE Categories
+	SET MOVIES_IN_CAT = MOVIES_IN_CAT + 1
+	WHERE CATEGORY = (SELECT DISTINCT CATEGORY FROM Movie WHERE Movie.CATEGORY=new.CATEGORY);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `inventory_add` AFTER INSERT ON `Movie` FOR EACH ROW BEGIN
+INSERT INTO Inventory(MOVIE_ID, STOCK, STOCK_OUT)
+VALUES (new.MOVIE_ID, 1, 0);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `inventory_remove` BEFORE DELETE ON `Movie` FOR EACH ROW BEGIN
+DELETE FROM Inventory
+    WHERE Inventory.MOVIE_ID = old.MOVIE_ID;
+END
+$$
+DELIMITER ;
 
 --
 -- Indexes for dumped tables
--- Rented.sql
+++ Rented.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: classmysql.engr.oregonstate.edu:3306
--- Generation Time: Aug 06, 2019 at 01:27 PM
+-- Generation Time: Aug 14, 2019 at 04:15 PM
 -- Server version: 10.3.13-MariaDB-log
 -- PHP Version: 7.0.33
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `cs340_smitbre2`
+-- Database: `cs340_mahlert`
 --
 
 -- --------------------------------------------------------
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `Rented` (
-  `ID` int(4) NOT NULL,
+  `USER_ID` int(4) NOT NULL,
   `MOVIE_ID` int(4) NOT NULL,
   `DATE` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -38,8 +38,29 @@ CREATE TABLE `Rented` (
 -- Dumping data for table `Rented`
 --
 
-INSERT INTO `Rented` (`ID`, `MOVIE_ID`, `DATE`) VALUES
-(8, 14, '2019-08-06 13:27:02');
+INSERT INTO `Rented` (`USER_ID`, `MOVIE_ID`, `DATE`) VALUES
+(13, 14, '2019-08-06 13:27:02'),
+(13, 121, '2019-08-07 11:17:07'),
+(123, 434, '2019-08-07 11:17:26'),
+(626, 121, '2019-08-07 11:17:46');
+
+--
+-- Triggers `Rented`
+--
+DELIMITER $$
+CREATE TRIGGER `update_stock_after_rent` AFTER INSERT ON `Rented` FOR EACH ROW BEGIN
+	UPDATE Inventory
+    SET STOCK = STOCK - 1, STOCK_OUT = STOCK_OUT + 1;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `update_stock_after_return` AFTER DELETE ON `Rented` FOR EACH ROW BEGIN
+	UPDATE Inventory
+    SET Stock = Stock + 1, STOCK_OUT = STOCK_OUT - 1;
+END
+$$
+DELIMITER ;
 
 --
 -- Indexes for dumped tables
@@ -49,8 +70,8 @@ INSERT INTO `Rented` (`ID`, `MOVIE_ID`, `DATE`) VALUES
 -- Indexes for table `Rented`
 --
 ALTER TABLE `Rented`
-  ADD PRIMARY KEY (`ID`,`MOVIE_ID`),
-  ADD KEY `ID` (`ID`);
+  ADD PRIMARY KEY (`USER_ID`,`MOVIE_ID`),
+  ADD KEY `ID` (`USER_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
