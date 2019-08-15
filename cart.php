89886c3beea6b82f76c38b194af84c7b083b3191
Thomas Mahler <mahlert@flip3.engr.oregonstate.edu>
2019-08-14 19:08:57 -0700

    Added renting functions with duplicate recognition.

--- cart.php
+++ cart.php
@@ -2,6 +2,52 @@
 <?php
 	include 'connectvars.php';
 	include 'header.php';
+
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if (!$conn) {
+		die('Could not connect: ' . mysql_error());
+	}
+
+	if(isset($_POST['save'])){
+
+    $movie_id = trim($_POST["movie_id"]);
+    if(empty($movie_id)){
+        $movie_id_err = "Please enter movie_id.";
+    } elseif(!ctype_digit($movie_id)){
+        $movie_id_err = "Please enter a positive integer value of id.";
+    }
+
+
+    $user_id = trim($_POST["user_id"]);
+    if(empty($user_id)){
+        $user_id_err = "Please enter user_id.";
+    } elseif(!ctype_digit($user_id)){
+        $user_id_err = "Please enter a positive integer value of id.";
+    }
+
+		if(empty($user_id__err) && empty($movie_id_err)){
+	        $sql = "INSERT INTO Rented (USER_ID, MOVIE_ID, CURR_DATE) VALUES('$user_id', '$movie_id', default)";
+
+	        if($stmt = mysqli_prepare($conn, $sql)){
+
+	            mysqli_stmt_bind_param($stmt, "isdi", $param_user_id, $param_movie_id);
+
+	            // Set parameters
+				      $param_user_id = $user_id;
+	            $param_movie_id = $movie_id;
+
+	            if(mysqli_stmt_execute($stmt)){
+	                header("location: index.php");
+	                exit();
+	            } else{
+	                echo "Duplicate record";
+						}
+					}
+		}
+	}
+
+	mysqli_close($conn);
+
 ?>
 
 <html>
@@ -11,62 +57,17 @@
 	</head>
 <body>
 		<form action="" method="POST">
-			<label>user_id</label>
-			<input type="text" name="user_id" value = "">
-			<label>password</label>
-			<input type="type2" name="password" value="">
-			<button type="submit" name = "add">Add</button>
+			<div>
+				<label>user_id</label>
+				<input type="text" name="user_id" value = "">
+			</div>
+			<div>
+				<label>movie_id</label>
+				<input type="text" name="movie_id" value="">
+			</div>
+			<button type="submit" name = "save">Rent!</button>
 		</form>
 
-<?php
-
-	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-	if (!$conn) {
-		die('Could not connect: ' . mysql_error());
-	}
-
-	if(isset($_POST['add'])){
-		$user_id=$_POST['user_id'];
-		$password=$_POST['password'];
-
-		$mysqli_query->query("INSERT INTO User (USER_ID, PASSWORD) VALUES('$user_id', '$password')");
-
-	}
-
-	$query = "SELECT * FROM User ";
-
-		$result = mysqli_query($conn, $query);
-		if (!$result) {
-			die("Query to show fields from table failed");
-		}
-
-		if(mysqli_num_rows($result) > 0){
-	        echo "<h1>User</h1>";
-			echo "<table class = 'table' id='t01' border='1'>";
-	        echo "<thead>";
-				echo "<tr>";
-				echo "<th>USER_ID</th>";
-				echo "<th>PASSWORD</th>";
-				echo "</tr>";
-	        echo "</thead>";
-	        echo "<tbody>";
-
-	        while($row = mysqli_fetch_array($result)){
-						echo "<tr>";
-						echo "<td>" . $row['USER_ID'] . "</td>";
-						echo "<td>" . $row['PASSWORD'] . "</td>";
-						echo "</tr>";
-	        }
-	        echo "</tbody>";
-	        echo "</table>";
-
-	        mysqli_free_result($result);
-	    } else{
-			echo "<p class='lead'><em>No records were found.</em></p>";
-	    }
-
-	mysqli_close($conn);
-?>
 </body>
 
 </html>
